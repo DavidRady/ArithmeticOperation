5da25b2a41cad9a5109ca108a6d03eb0bdc2381c
David Rady <49375043+DavidRady@users.noreply.github.com>
2021-03-30 23:18:58 -0400

    Update ArithmeticOperationsTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -4,19 +4,17 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.fail;
-
 public class ArithmeticOperationsTest {
 
 
-// 	@Test
-// 	public void testAdd()
-// 	{
+	@Test
+	public void testAdd()
+	{
 
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		//assertEquals(expected, actual);
+		// assertEquals(expected, actual);
 		fail("must fail");
 
 	}
